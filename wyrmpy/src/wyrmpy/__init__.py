97d91b3bb4b8ac6bdaea1904285cbc0224d2ceb8
nwsutton <nwsutton@ncsu.edu>
2026-06-25 14:36:51 -0400

    ADded wsl installer

--- wyrmpy/src/wyrmpy/__init__.py
+++
@@ -1,2 +0,0 @@
-from .types import WyrmFrame, WyrmRigidBody, WyrmDescription
-from .serialization import deserialize_frame, deserialize_descriptions
--- wyrmpy/src/wyrmpy/serialization.py
+++
@@ -1,8 +0,0 @@
-from ._parser import parse_frame, parse_descriptions
-from .types import WyrmFrame, WyrmDescription
-
-def deserialize_frame(data: bytes) -> WyrmFrame:
-    return parse_frame(data)
-
-def deserialize_descriptions(data: bytes) -> dict[int, WyrmDescription]:
-    return parse_descriptions(data)
